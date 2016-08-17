0383aa924ae642686cb8e06873e9b1b1aa9d1ff4
Joel McCoy <joelmccoy+github@gmail.com>
2016-08-16 23:42:41 -0400

    Move lists to corpora/*.json

--- bots.rb
+++ bots.rb
@@ -9,29 +9,24 @@ require_relative 'queneau'
 
 SETTINGS = Dotenv.load.merge(ENV)
 
-GAME_ENGINES = ["Pyrogenesis", "Aleph One", "Clausewitz", "RPG Maker", "Ren'Py", "PlayN", "Starling Framework", "Real Virtuality", "Cube", "Spring", "Infinity Engine", "Buildbox", "Enigma Engine", "Stratagus", "IW engine", "BRender", "Adventure Game Studio", "Core3D", "Moai SDK", "Cube 2", "Jedi", "Kivy", "id Tech 4", "id Tech 1","Source 2", "Riot Engine", "Shark 3D", "Blend4Web", "Luminous Studio", "Anura", "Source", "GoldSrc", "Cocos2d, Cocos2d-x, Cocos2d-html5", "GameMaker: Studio", "StepMania", "TOSHI", "Crystal Space", "ORX", "Adventure Game Interpreter", "Anvil", "BigWorld", "Bork3D Game Engine", "C4 Engine", "Chrome Engine", "CryEngine", "Crystal Tools", "Dagor Engine", "Dunia Engine", "ego", "Flare3D", "Fox Engine", "Freescape", "Frostbite", "Gamebryo", "Jade", "LithTech", "LyN", "MT Framework", "PhyreEngine", "Pie in the Sky", "Rockstar Advanced Game Engine","SAGE", "Unigine", "V-Play Game Engine", "Vicious Engine", "Vision", "RenderWare", "Unity", "Unreal Engine", "SCUMM", "Torque3D", "LÖVE", "4A Engine", "VRAGE", "ONScripter", "Game Editor", "OpenClonk", "HPL Engine", "Coldstone", "Panta Rhei", "Turbulenz", "ShiVa", "id Tech 2","id Tech 2","id Tech 3", "id Tech 5", "UbiArt Framework", "Alamo", "Odyssey Engine", "PlayCanvas", "Creation Engine", "REDengine", "Panda3D", "OGRE", "DimensioneX Multiplayer Engine", "Flexible Isometric Free Engine", "ioquake3", "Flixel", "Sierra's Creative Interpreter","Blender"]
-NUMPAD_ARROWS = [nil,'↙','↓','↘','←',nil,'→','↖','↑','↗']
-ALL_ARROWS = ["\u2190","\u2191","\u2192","\u2193","\u2194","\u2195","\u2196","\u2197","\u2198","\u2199","\u25b2","\u25bc","\u25c0","\u25b6","\u2794","\u2798","\u2799","\u279a","\u279b","\u279c","\u279d","\u279e","\u279f","\u27a0","\u27a1","\u27a2","\u27a3","\u27a4","\u27a5","\u27a6","\u21aa","\u21a9","\u219a","\u219b","\u219c","\u219d","\u219e","\u219f","\u21a0","\u21a1","\u21a2","\u21a3","\u21a4","\u21a6","\u21a5","\u21a7","\u21a8","\u21ab","\u21ac","\u21ad","\u21ae","\u21af","\u21b0","\u21b1","\u21b2","\u21b4","\u21b3","\u21b5","\u21b6","\u21b7","\u21b8","\u21b9","\u21ba","\u21bb","\u27f2","\u27f3","\u21bc","\u21bd","\u21be","\u21bf","\u21c0","\u21c1","\u21c2","\u21c3","\u21c4","\u21c5","\u21c6","\u21c7","\u21c8","\u21c9","\u21ca","\u21cb","\u21cc","\u21cd","\u21cf","\u21cf","\u21cf","\u21cf","\u21cf","\u21cf","\u21cf","\u21d5","\u21d6","\u21d7","\u21d8","\u21d9","\u21d9","\u21f3","\u21da","\u21db","\u21dc","\u21dd","\u21de","\u21df","\u21df","\u21df","\u21e0","\u21e1","\u21e2","\u21e3","\u21e4","\u21e5","\u21e6","\u21e8","\u21e9","\u21ea","\u21e7","\u21eb","\u21ec","\u21ed","\u21ee","\u21ef","\u21f0","\u21f1","\u21f2","\u21f4","\u21f5","\u21f6","\u21f7","\u21f8","\u21f9","\u21fa","\u21fa","\u21fb","\u21fc","\u21ff","\u27f0","\u27f1","\u27f4","\u27f5","\u27f6","\u27f7","\u27f8","\u27f9","\u27fd","\u27fe","\u27fa","\u27fb","\u27fc","\u27ff","\u2900","\u2901","\u2905","\u2902","\u2903","\u2904","\u2906","\u2907","\u2908","\u2909","\u290a","\u290b","\u290c","\u290d","\u290e","\u290f","\u2910","\u2911","\u2912","\u2913","\u2914","\u2915","\u2916","\u2917","\u2918","\u2919","\u2919","\u291a","\u291b","\u291c","\u291d","\u291e","\u2921","\u2922","\u2923","\u2924","\u2925","\u2926","\u2927","\u2928","\u2929","\u292a","\u292d","\u292e","\u292f","\u2930","\u2931","\u2932","\u2933","\u293b","\u2938","\u293e","\u293f","\u293a","\u293c","\u293d","\u2934","\u2935","\u2936","\u2937","\u2939","\u2940","\u2941","\u2942","\u2943","\u2944","\u2945","\u2946","\u2947","\u2948","\u2949","\u2952","\u2953","\u2954","\u2955","\u2956","\u2957","\u2958","\u2959","\u295a","\u295b","\u295c","\u295d","\u295e","\u295f","\u2960","\u2961","\u2962","\u2963","\u2964","\u2965","\u2966","\u2967","\u2968","\u2969","\u296a","\u296b","\u296c","\u296d","\u296e","\u296f","\u2970","\u2971","\u2972","\u2973","\u2974","\u2975","\u2976","\u2977","\u2978","\u2979","\u297a","\u297b","\u27a7","\u27a8","\u27a9","\u27aa","\u27ab","\u27ac","\u27ad","\u27ae","\u27af","\u27b1","\u27b2","\u27b3","\u27b4","\u27b5","\u27b6","\u27b7","\u27b8","\u27b9","\u27ba","\u27bb","\u27bc","\u27bd","\u27be","\u2b05","\u2b06","\u2b07","\u23ce","\u2b0e","\u2b0f","\u2b10","\u2b11","\u2608","\u2607","\u2343","\u2344","\u2347","\u2348","\u2350","\u2357","\u234c","\u2353","\u234d","\u2354","\u234f","\u2356","\u2345","\u2346","\u2b08","\u2b09","\u2b0a","\u2b0b","\u2b0c","\u2b0d","\u2b00","\u2b01","\u2b02","\u2b03","\u2b04"]
-MOVES = JSON.parse(File.read('moves.json'))
-NUM_INPUTS = ["236", "623", "214", "421", "41236", "6237", "4219", "63214", "632146", "89632147", "8963214789632147", "412369", "632147", "2141236", "2363214", "21412364", "23632146", "[4]6", "[1]9", "[2]8", "[3]7", "[4]646", "[1]919", "[2]828", "[3]737", "4123641236", "6321463214", "4123632146", "6321412364"]
-ATTACK_COMMANDS = ["P", "K", "S", "HS", "D", "LP", "MP", "HP", "LK", "MK", "HK", "PPP", "KKK"]
-BUTTONS = {
-  "1": '↙',
-  "2": '↓',
-  "3": '↘',
-  "4": '←',
-  "6": '→',
-  "7": '↖',
-  "8": '↑',
-  "9": '↗',
-  P: "\u24C5",
-  K: "\u24C0",
-  H: "\u24BD",
-  S: "\u24C8",
-  M: "\u24C2",
-  D: "\u24B9",
-  L: "\u24C1"
-}
+def read_json_file(path)
+  begin
+    json_string = File.read(path)
+
+  rescue Errno::ENOENT
+    path = "corpora/#{path}.json"
+  end
+
+  JSON.parse(File.read(path))
+end
+
+GAME_ENGINES = read_json_file("game_engines")
+NUMPAD_ARROWS = read_json_file("numpad_arrows")
+ALL_ARROWS = read_json_file("all_arrows")
+MOVES = read_json_file("moves")
+NUM_INPUTS = read_json_file("num_inputs")
+ATTACK_COMMANDS = read_json_file("attack_commands")
+BUTTONS = read_json_file("buttons")
 
 class MyBot < Ebooks::Bot
   attr_accessor 'gb_api_key', 'gb_api', 'gb_params'
@@ -81,15 +76,15 @@ class MyBot < Ebooks::Bot
     self.video_types_max = 14
 
     # Bespoke, artisinal contents
-    @title_hashtags = ["#NameYourJunkAfterAGame", "#DescribeYourSexLifeWithAGame"]
-    @interjections = ["lol", "ayyyy lmao", "wtf right?!", "tho", "lmfao", "lmaoooooo", "iM DyING", "*SCREAMING*", "#TRUTH", "#LIFE", "#blessed"]
-    @softeners = ["imo", "tbqh", "tho", "though", "prolly", "probably", "I think", "just saying", "sorry", "sorry, not sorry"]
-    @sins = ["romanced CHAR", "slept with CHAR", "had carnal relations with CHAR", "left CHAR to die", "never cared about CHAR", "cried irl over CHAR", "RPed as CHAR", "LARPed as CHAR", "shipped myself with CHAR", "a shrine to CHAR", "coveted my neighbor's CHAR", "taken CHAR's name in vain", "not one fuck to give about CHAR", "known CHAR...biblically."]
     @engines = GAME_ENGINES
     @moves = MOVES
     @moves_qb = Queneau.new(@moves)
-    @trivia = ["did you know?", "#tmyk", "wow!", "I can't believe", "you won't believe", "can you believe", "were you aware", "it's amazing that", "who could guess", "just learned", "TILT:", "oh wow", "amazing!", "NO WAY!"]
-    @solo_activities = ["smoked weed", "got drunk", "took you to prom", "got pregnant", "took shrooms", "tripped balls", "went to ur school", "did let's plays", "proposed to you", "were gay", "were gay as hell"]
+    @title_hashtags = read_json_file("title_hashtags")
+    @interjections = read_json_file("interjections")
+    @softeners = read_json_file("softeners")
+    @sins = read_json_file("sins")
+    @trivia = read_json_file("trivia")
+    @solo_activities = read_json_file("solo_activities")
     @memes = [
       {label: 'bogost_games', action: :tweet, gen: proc {
         "#{get_game_title} would be a lot better without characters #{softeners.sample}"
